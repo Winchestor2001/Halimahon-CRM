05376f41ac7604172db4156bfb9568bb98738564
Bexruz Sayfiddinov <75200506+Winchestor2001@users.noreply.github.com>
2024-11-24 12:24:07 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -118,15 +118,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'halimahon_db',
-         'USER': 'halimahon_user',
-         'PASSWORD': '8halimahon8',
-         'HOST': 'localhost',
-         'PORT': 5432,
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#          'NAME': 'halimahon_db',
+#          'USER': 'halimahon_user',
+#          'PASSWORD': '8halimahon8',
+#          'HOST': 'localhost',
+#          'PORT': 5432,
+#      }
+# }
 
 
 # Password validation
